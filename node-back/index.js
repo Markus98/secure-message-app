b9d670b4c33dfaa8b4aadfda44ef6be3284fa785
Markus Tuominen <maketuominen@hotmail.com>
2021-03-16 16:19:04 +0200

    express hello world init and nodemon

---
+++ node-back/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
