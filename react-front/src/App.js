940d7178f713aa585a3a0dc4c207eb38e4e15f19
Saurus <313.saurus@gmail.com>
2021-03-26 21:05:18 +0200

    add try catch on axios response

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   Switch, Route, useRouteMatch
 } from 'react-router-dom';
 
-import messageService from './services/messages';
 import MessageForm from './components/MessageForm';
 import MessageView from './components/MessageView';
 
-//dont bother checking this, just renders the messages into frontend.
 function App() {
-
   const messageMatch = useRouteMatch('/:id');
-  //crashes on wrong id, since no way to know if messagematch exists
   return (
     <div className='App'>
       <Switch>
--- react-front/src/services/messages.js
+++ react-front/src/services/messages.js
@@ -14,36 +14,43 @@ const create_message = async (msg, password, lifetime, readLimit) => {
     {
         jsonObj.readLimit = readLimit;
     }
-    const response = await axios.post(baseURL, jsonObj);
-    console.log(response);
-    if(response.status === 400) {
-        console.log('No message provided in the request');
+    try {
+        const response = await axios.post(baseURL, jsonObj);
+        console.log(response);
+
+    } catch (error) {
+        const status = error.response.status;
+        if(status === 400) {
+            console.log('No message provided in the request');
+        } else {
+            console.log(error);
+        }
         return null;
-    } else if(response.status === 200) {
-        console.log('Message created');
-        return response.data;
     }
-    return null;
 }
 
 const get_message = async (msgURL, password) => {
-    const response = await axios.get(`${baseURL}/{msgURL}`, {password: password});
-    console.log(response);
-    if(response.status === 404) {
-        console.log(`No message exist with URL: ${msgURL}`);
-        return {exist: false};
-    } else if(response.status === 401) {
-        console.log(`Message with URL: ${msgURL} is password protected`);
-        return {exist: true};
-    } else if(response.status === 403) {
-        console.log('Invalid password');
-        return {exist: true};
-    } else if(response.status === 200) {
-        console.log('Successfully fetched the message');
+    try {
+        const response = await axios.get(`${baseURL}/{msgURL}`, {password: password});
+        console.log(response);
         response.data.exist = true;
         return response.data;
+    } catch (error) {
+        const status = error.response.status;
+        if(status === 404) {
+            console.log(`No message exist with URL: ${msgURL}`);
+            return {exist: false};
+        } else if(status === 401) {
+            console.log(`Message with URL: ${msgURL} is password protected`);
+            return {exist: true};
+        } else if(status === 403) {
+            console.log('Invalid password');
+            return {exist: true};
+        } else {
+            console.log(error);
+            return null;
+        }
     }
-    return null;
 }
 
 const delete_message = async (msgURL, password) => {
