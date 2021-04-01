020bff5f8189de1cb52ef92d4fb987b94655cf4a
Markus Tuominen <maketuominen@hotmail.com>
2021-04-01 22:19:44 +0300

    Backend hosting for built frontend now works

--- node-back/index.js
+++ node-back/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+const path = require('path');
+
 //controller for router for interacting with database
 let messageRoute = require("./controllers/messages");
 app.use(cors());
 app.use(express.json());
-//notice its localhost/api
+
 app.use('/api',messageRoute);
+// For hosting the built frontend
+app.use(express.static("build"));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
