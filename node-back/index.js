239f1d4e2a5a01ce1072730308113426bdaf0929
Saurus <313.saurus@gmail.com>
2021-04-16 11:36:21 +0300

    Lari change

--- node-back/index.js
+++ node-back/index.js
@@ -17,7 +17,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
